efd98fd45441c0c053c5e2dbc12bd3c3c8fec9dd
reticent67 <rrafter36058@gmail.com>
2017-09-18 23:57:47 -0700

    Added Intake components and progbar

--- CrimeReport/ClientApp/app/app.module.shared.ts
+++ CrimeReport/ClientApp/app/app.module.shared.ts
@@ -1,42 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { AppRoutes } from './app.routes';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { MainMenuComponent } from './components/mainmenu/mainmenu.component';
+import { NewCRStartComponent } from './components/newCrimeReport/newCR.start.component';
+import { NewCRProgBarComponent } from './components/newCrimeReport/newCR.progbar.component';
+import { NewCRDetailsComponent } from './components/newCrimeReport/newCR.details.component';
 
-//
-import { MainMenuComponent}  from './components/mainmenu/mainmenu.component';
+
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent,
         MainMenuComponent,
+        NewCRStartComponent,
+        NewCRProgBarComponent,
+        NewCRDetailsComponent
+        
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
+        ReactiveFormsModule,
         BrowserAnimationsModule, 
         MaterialModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'main-menu', pathMatch: 'full' },
-            { path: 'main-menu', component: MainMenuComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
+        AppRoutes
     ]
 })
 export class AppModuleShared {
---
+++ CrimeReport/ClientApp/app/app.routes.ts
@@ -0,0 +1,22 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import { MainMenuComponent } from './components/mainmenu/mainmenu.component';
+import { NewCRStartComponent } from './components/newCrimeReport/newCR.start.component';
+import { NewCRDetailsComponent } from './components/newCrimeReport/newCR.details.component';
+//import { IntakePeopleComponent } from './components/intake/intake.people.component';
+
+
+export const routes: Routes = [
+    { path: 'main-menu', component: MainMenuComponent },
+    {
+        path: 'newCR-start', component: NewCRStartComponent,
+        children: [
+           { path: '', redirectTo: 'newCR-details', pathMatch: 'full' },
+           { path: 'newCR-details', component: NewCRDetailsComponent },
+    //        { path: 'intake-people', component: IntakePeopleComponent },
+       ]
+    },
+    {path: '**', redirectTo: 'main-menu'}
+  
+];
+
+export const AppRoutes = RouterModule.forRoot(routes);
